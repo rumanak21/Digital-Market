0e2f14071cc38af572d2f688e491b3da0c105f34
rumanak21 <rumana2k20@gmail.com>
2024-01-23 12:00:14 +0100

    login form creating using payload cms

---
+++ src/get-payload.ts
@@ -0,0 +1,48 @@
+import dotenv from "dotenv"
+import path from "path"
+import payload from "payload"
+import { InitOptions } from "payload/config"
+dotenv.config({
+    path: path.resolve(__dirname, "../.env")
+})
+
+let cashed = (global as any).payload
+
+if (!cashed) {
+    cashed = (global as any).payload = {
+        client: null,
+        promise: null,
+    }
+}
+
+interface Args {
+    initOptions?: Partial<InitOptions>
+}
+
+
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+    if (!process.env.PAYLOAD_SECRET) {
+        throw new Error("PAYLOAD_SECRET is missing")
+    }
+    if (cashed.client) {
+        return cashed.client
+    }
+    if (!cashed.promise) {
+        cashed.promise = payload.init({
+            secret: process.env.PAYLOAD_SECRET,
+            local: initOptions?.express ? false : true,
+            ...(initOptions || {})
+        })
+    }
+
+    try {
+        cashed.client = await cashed.promise
+        
+    } catch (e: unknown) {
+        cashed.promise = null
+        throw e
+    }
+
+    return cashed.client
+
+}
